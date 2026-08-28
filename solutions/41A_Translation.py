098918edb503381627554527a36607222284ed3f
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:43 +0300

    Auto-sync: [41A] Translation

--- solutions/41A_Translation.py
+++ solutions/41A_Translation.py
@@ -4,10 +4,10 @@ Codeforces Submission ID: 388531728
 Author: habebaabdelrahem4
 """
 
-s = input().strip()
-t = input().strip()
-
-if s[::-1] == t:
-    print("YES")
-else:
+s = input().strip()
+t = input().strip()
+
+if s[::-1] == t:
+    print("YES")
+else:
     print("NO")
