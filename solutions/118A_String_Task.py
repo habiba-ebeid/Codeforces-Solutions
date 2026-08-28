2e7381f64e0512f5098ed166c9bf68bc552359c4
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:55:01 +0300

    Auto-sync: [118A] String Task

--- solutions/118A_String_Task.py
+++ solutions/118A_String_Task.py
@@ -1,16 +1,16 @@
 """
 Problem: String Task (118A)
-Codeforces Submission ID: 388417264
+Codeforces Submission ID: 388623034
 Author: habebaabdelrahem4
 """
 
-s = input().strip().lower()
-
-vowels = "aoyeui"
-result = ""
-
-for char in s:
-    if char not in vowels:
-        result += "." + char
-
-print(result)
+s = input().strip().lower()
+
+vowels = "aoyeui"
+result = []
+
+for char in s:
+    if char not in vowels:
+        result.append('.' + char)
+
+print(''.join(result))
