c6c512a5162f0ffad8b01ef47776f395c70d6901
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:05 +0300

    Auto-sync: [677A] Vanya and Fence

--- solutions/677A_Vanya_and_Fence.py
+++ solutions/677A_Vanya_and_Fence.py
@@ -4,15 +4,15 @@ Codeforces Submission ID: 388531229
 Author: habebaabdelrahem4
 """
 
-n, h = map(int, input().split())
-heights = list(map(int, input().split()))
-
-total_width = 0
-
-for a in heights:
-    if a > h:
-        total_width += 2
-    else:
-        total_width += 1
-
+n, h = map(int, input().split())
+heights = list(map(int, input().split()))
+
+total_width = 0
+
+for a in heights:
+    if a > h:
+        total_width += 2
+    else:
+        total_width += 1
+
 print(total_width)
