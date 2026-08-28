86d3b0289f8f18e14ad9136995ab03643b5da591
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:42 +0300

    Auto-sync: [467A] George and Accommodation

--- solutions/467A_George_and_Accommodation.py
+++ solutions/467A_George_and_Accommodation.py
@@ -4,13 +4,13 @@ Codeforces Submission ID: 388532362
 Author: habebaabdelrahem4
 """
 
-n = int(input())
-
-count = 0
-
-for _ in range(n):
-    p, q = map(int, input().split())
-    if q - p >= 2:
-        count += 1
-
+n = int(input())
+
+count = 0
+
+for _ in range(n):
+    p, q = map(int, input().split())
+    if q - p >= 2:
+        count += 1
+
 print(count)
