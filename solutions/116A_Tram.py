d0c95eac0b1b5cb1086f9bc1fd90f9418ded5b0f
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:29 +0300

    Auto-sync: [116A] Tram

--- solutions/116A_Tram.py
+++ solutions/116A_Tram.py
@@ -4,14 +4,14 @@ Codeforces Submission ID: 388531532
 Author: habebaabdelrahem4
 """
 
-n = int(input())
-
-current_passengers = 0
-max_capacity = 0
-
-for _ in range(n):
-    a, b = map(int, input().split())
-    current_passengers = current_passengers - a + b
-    max_capacity = max(max_capacity, current_passengers)
-
+n = int(input())
+
+current_passengers = 0
+max_capacity = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    current_passengers = current_passengers - a + b
+    max_capacity = max(max_capacity, current_passengers)
+
 print(max_capacity)
