75356dba1056565c0d13d6323f8feac4a66c2730
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:04 +0300

    Auto-sync: [1030A] In Search of an Easy Problem

--- solutions/1030A_In_Search_of_an_Easy_Problem.py
+++ solutions/1030A_In_Search_of_an_Easy_Problem.py
@@ -4,10 +4,10 @@ Codeforces Submission ID: 388531982
 Author: habebaabdelrahem4
 """
 
-n = int(input())
-responses = input().split()
-
-if '1' in responses:
-    print("HARD")
-else:
+n = int(input())
+responses = input().split()
+
+if '1' in responses:
+    print("HARD")
+else:
     print("EASY")
