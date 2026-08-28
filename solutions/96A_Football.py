7ace92b45af4d6a077bd748ece1aee381426e5eb
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:54:04 +0300

    Auto-sync: [96A] Football

---
+++ solutions/96A_Football.py
@@ -0,0 +1,12 @@
+"""
+Problem: Football (96A)
+Codeforces Submission ID: 388623897
+Author: habebaabdelrahem4
+"""
+
+s = input().strip()
+
+if "0000000" in s or "1111111" in s:
+    print("YES")
+else:
+    print("NO")
