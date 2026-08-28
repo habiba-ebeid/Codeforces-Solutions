a8f9cd48ddf017c886645293117b21d4165da8ca
habebaabdelrahem4 <habebaabdelrahem4@users.noreply.github.com>
2026-08-28 04:29:05 +0300

    Add solution for [69A] Young Physicist

---
+++ solutions/69A_Young_Physicist.py
@@ -0,0 +1,22 @@
+"""
+Problem: Young Physicist (69A)
+Codeforces Submission ID: 388623168
+Author: habebaabdelrahem4
+"""
+
+n = int(input())
+
+sum_x = 0
+sum_y = 0
+sum_z = 0
+
+for _ in range(n):
+    x, y, z = map(int, input().split())
+    sum_x += x
+    sum_y += y
+    sum_z += z
+
+if sum_x == 0 and sum_y == 0 and sum_z == 0:
+    print("YES")
+else:
+    print("NO")
