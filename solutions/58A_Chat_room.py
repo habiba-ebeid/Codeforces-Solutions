60083d768205fe87bf5bd9a5e5ec51b87e5b178a
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-08-28 03:56:01 +0300

    Auto-sync: [58A] Chat room

---
+++ solutions/58A_Chat_room.py
@@ -0,0 +1,21 @@
+"""
+Problem: Chat room (58A)
+Codeforces Submission ID: 388624591
+Author: habebaabdelrahem4
+"""
+
+s = input().strip()
+
+target = "hello"
+index = 0
+
+for char in s:
+    if char == target[index]:
+        index += 1
+        if index == len(target):
+            break
+
+if index == len(target):
+    print("YES")
+else:
+    print("NO")
