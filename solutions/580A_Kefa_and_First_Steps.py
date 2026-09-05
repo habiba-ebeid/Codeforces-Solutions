d30fb8f6d7852cdbc794f1d3753b9b8cdb0f04ee
Habiba Ebeid <habebaabdelrahem4@gmail.com>
2026-09-05 11:07:51 +0300

    Auto-sync: [580A] Kefa and First Steps

---
+++ solutions/580A_Kefa_and_First_Steps.py
@@ -0,0 +1,30 @@
+"""
+Problem: Kefa and First Steps (580A)
+Codeforces Submission ID: 389432786
+Author: habebaabdelrahem4
+"""
+
+"""
+Codeforces - 580A: Kefa and First Steps
+By: Habiba
+"""
+
+def solve() -> None:
+    days_count = int(input())
+    earnings = list(map(int, input().split()))
+
+    current_streak = 1
+    best_streak = 1
+
+    for day in range(1, days_count):
+        if earnings[day] >= earnings[day - 1]:
+            current_streak += 1
+            if current_streak > best_streak:
+                best_streak = current_streak
+        else:
+            current_streak = 1
+
+    print(best_streak)
+
+if __name__ == "__main__":
+    solve()
